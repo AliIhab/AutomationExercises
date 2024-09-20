5e51987eb6f5cff2ed3a3b9900bcf67c191290fb
AliIhab <ali_ihab@yahoo.com>
2024-09-20 22:36:08 +0300

    Updated Commit

--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -1,7 +1,8 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
@@ -20,19 +21,30 @@ public class LoginPageTest {
         driver.manage().window().maximize();
         driver.get("https://www.automationexercise.com/login");
     }
-    @Test (priority = 8)
+    @Test (priority = 2)
     public void ValidLogin(){
+        By login = By.xpath("//body[1]/header[1]/div[1]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[10]/a[1]");
         loginPageObject = new LoginPage(driver);
         loginPageObject.enterEmailLogin("Ali3@example.com");
         loginPageObject.enterPasswordLogin("password123");
         loginPageObject.pressLogin();
+        //condition for valid login
+        Assert.assertTrue(driver.findElement(login).isDisplayed());
+
     }
-    @Test
+    @Test (priority = 4)
+    public void SignoutTest(){
+        loginPageObject = new LoginPage(driver);
+        loginPageObject.pressSignout();
+        loginPageObject.signoutConfirmation();
+    }
+    @Test (priority = 6)
     public void InvalidLogin(){
         loginPageObject = new LoginPage(driver);
         loginPageObject.enterEmailLogin("Ali3@example.com");
         loginPageObject.enterPasswordLogin("wrongpassword");
         loginPageObject.pressLogin();
+
     }
 
 
