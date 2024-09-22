4f1ddbf56be51bb6b48130c3446d300d664295e9
AliIhab <ali_ihab@yahoo.com>
2024-09-22 16:07:06 +0300

    Updated Commit

--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-
 import java.time.Duration;
 import Pages.LoginPage;
 import org.testng.annotations.Test;
@@ -14,6 +13,7 @@ public class LoginPageTest {
     WebDriver driver;
     LoginPage loginPageObject;
 
+
     @BeforeTest
     public void setDriver(){
         WebDriverManager.chromedriver().setup();
@@ -21,6 +21,7 @@ public class LoginPageTest {
         driver.manage().window().maximize();
         driver.get("https://www.automationexercise.com/login");
     }
+
     @Test (priority = 2)
     public void ValidLogin(){
         By login = By.xpath("//body[1]/header[1]/div[1]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[10]/a[1]");
@@ -32,18 +33,31 @@ public class LoginPageTest {
         Assert.assertTrue(driver.findElement(login).isDisplayed());
 
     }
+
     @Test (priority = 4)
     public void SignoutTest(){
-        loginPageObject = new LoginPage(driver);
         loginPageObject.pressSignout();
         loginPageObject.signoutConfirmation();
     }
+
+    @Test (priority = 5)
+    public void SignupWithExistingMail(){
+        By SignupConf = By.xpath("//p[contains(text(),'Email Address already exist!')]");
+        driver.get("https://www.automationexercise.com/login");
+        loginPageObject.enterEmailSignup("Ali3@example.com");
+        loginPageObject.enterUsername("Ali Ihab");
+        loginPageObject.pressSignup();
+        Assert.assertTrue(driver.findElement(SignupConf).isDisplayed());
+    }
+
     @Test (priority = 6)
     public void InvalidLogin(){
+        By WrongPass = By.xpath("//p[contains(text(),'Your email or password is incorrect!')]");
         loginPageObject = new LoginPage(driver);
         loginPageObject.enterEmailLogin("Ali3@example.com");
         loginPageObject.enterPasswordLogin("wrongpassword");
         loginPageObject.pressLogin();
+        Assert.assertTrue(driver.findElement(WrongPass).isDisplayed());
 
     }
 
